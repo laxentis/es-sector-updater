e0a4a7f83efe8d147c4bf39481b47aab9d762625
laxentis <laxentis@gmail.com>
2022-12-31 01:16:42 +0100

    Sector file download

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use error_chain::error_chain;
+use reqwest::header;
+use reqwest::header::HeaderMap;
+use reqwest::redirect;
+use std::io::copy;
+use std::io::Cursor;
+use std::fs::File;
+use tempfile::Builder;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+const VERSION:&str = env!("CARGO_PKG_VERSION");
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    println!("ES Sector Update version {}", VERSION);
+    println!("Getting sector link");
+    let website = reqwest::get(
+        "http://files.aero-nav.com/EPWW"
+    ).await?.text().await?;
+    let document = scraper::Html::parse_document(&website);
+    let link_selector = scraper::Selector::parse("td>a").unwrap();
+    let links = document.select(&link_selector).map(|x| x.value().attr("href").unwrap()).filter(|x| is_correct_link(x, "Sector_AIRAC_Update", "zip"));
+    let file_url = links.last().unwrap();
+    println!("Got url: {}", file_url);
+    let tmp_dir = Builder::new().prefix("es-sector-updater-").tempdir()?;
+    let redirect_policy = redirect::Policy::custom(|attempt| attempt.stop());
+    let mut hdr = HeaderMap::new();
+    hdr.insert(header::ACCEPT, header::HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"));
+    hdr.insert(header::ACCEPT_ENCODING, header::HeaderValue::from_static("gzip, deflate, br"));
+    hdr.insert(header::ACCEPT_LANGUAGE, header::HeaderValue::from_static("en-US;q=0.7,en;q=0.3"));
+    hdr.insert(header::CONNECTION, header::HeaderValue::from_static("keep-alive"));
+    hdr.insert(header::DNT, header::HeaderValue::from_static("1"));
+    hdr.insert(header::HOST, header::HeaderValue::from_static("files.aero-nav.com"));
+    hdr.insert(header::REFERER, header::HeaderValue::from_static("http://files.aero-nav.com/"));
+    hdr.insert("Sec-Fetch-Dest", header::HeaderValue::from_static("document"));
+    hdr.insert("Sec-Fetch-Mode", header::HeaderValue::from_static("navigate"));
+    hdr.insert("Sec-Fetch-Site", header::HeaderValue::from_static("cross-site"));
+    hdr.insert("Sec-Fetch-User", header::HeaderValue::from_static("?1"));
+    hdr.insert(header::UPGRADE_INSECURE_REQUESTS, header::HeaderValue::from_static("1"));
+    hdr.insert(header::USER_AGENT, header::HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"));
+    
+    let client = reqwest::Client::builder().redirect(redirect_policy).default_headers(hdr).build()?;
+    let response = client.get(file_url).send().await?;
+    //println!("{:?}", response);
+    let file_name = tmp_dir.path().join("temp_sector.zip");
+    println!("Creating file: {}", file_name.display());
+    let mut file = File::create(file_name)?;
+    let mut content = Cursor::new(response.bytes().await?);
+    copy(&mut content, &mut file)?;
+
+    Ok(())
+}
+
+fn is_correct_link(link: &str, package_name: &str, format: &str) -> bool {
+    return link.contains(package_name) && link.ends_with(format)
+}
